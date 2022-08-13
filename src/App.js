93b4d80111219d685951a985d82891bd5f79865f
Yudi Supriyadi <yudi.s@photon.com>
2022-08-13 10:56:53 +0700

    Example React Table with sorting capability

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import TruckList from './pages/TruckList';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <TruckList />
   );
 }
 
