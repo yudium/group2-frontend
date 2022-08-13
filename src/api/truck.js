1aa5d43ad9e544d92337bc4faec01beda3defce9
Yudi Supriyadi <yudi.s@photon.com>
2022-08-13 14:01:09 +0700

    feat: task #1

--- src/api/truck.js
+++ src/api/truck.js
@@ -1,4 +1,48 @@
+import axios from 'axios';
+
+const api = 'http://localhost:5000'
+
 class Truck {
+    constructor(licenseNumber, licenseType, truckType, productionYear, stnk, kir) {
+        this.licenseNumber = licenseNumber
+        this.licenseType = licenseType
+        this.truckType = truckType
+        this.productionYear = productionYear
+        this.stnk = stnk
+        this.kir = kir
+    }
+    getTruck = () => {
+        return axios.get(`${api}/truck`).then(res => {
+            console.log(res)
+            this.licenseNumber = res.data.licenseNumber
+            this.licenseType = res.data.licenseType
+            this.truckType = res.data.truckType
+            this.productionYear = res.data.productionYear
+            this.stnk = res.data.stnk
+            this.kir = res.data.kir
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+    addTruck = () => {
+        return axios.post(`${api}/truck`, {
+            licenseNumber: this.licenseNumber,
+            licenseType: this.licenseType,
+            truckType: this.truckType,
+            productionYear: this.productionYear,
+            stnk: this.stnk,
+            kir: this.kir
+        })
+    }
+    updateTruck = () => {
+        return axios.put(`${api}/truck`, {
+            licenseNumber: this.licenseNumber,
+            licenseType: this.licenseType,
+            truckType: this.truckType,
+            productionYear: this.productionYear,
+        })
+    }
+
   async getTruckTypes() {
     return ['truck_type1', 'truck_type2'];
   }
@@ -43,6 +87,6 @@ class Truck {
   }
 }
 
-const truck = new Truck();
+const truck = new Truck()
 
-export default truck;
+export default truck
